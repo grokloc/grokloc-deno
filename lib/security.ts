be05fc23b7f4ee6a86d0fce3a5af1697bffea2d3
Brad Clawsie <brad@b7j0c.org>
2023-02-25 13:12:41 -0600

    rename funcs

--- lib/security.ts
+++ lib/security.ts
@@ -10,27 +10,27 @@ import {
  * @param {string} s - the input string
  * @returns {Uint8Array} the byte array representation of s
  */
-const textEncode = (s: string): Uint8Array => new TextEncoder().encode(s);
+const stringToBytes = (s: string): Uint8Array => new TextEncoder().encode(s);
 
 /**
  * convert a byte array to its string representation
  * @param {Uint8Array} d - the input byte array
  * @returns {string} the string representation of s
  */
-const textDecode = (d: Uint8Array): string => new TextDecoder().decode(d);
+const bytesToString = (d: Uint8Array): string => new TextDecoder().decode(d);
 
 /**
  * convert a byte array to its hex string encoding
  * @param {Uint8Array} d - the input byte array
  * @returns {string} the hex string encoding of d
  */
-const bytes2Hex = (d: Uint8Array): string => textDecode(hexEncode(d));
+const bytesToHex = (d: Uint8Array): string => bytesToString(hexEncode(d));
 
 /**
  * convert a hex string to its byte array representation
  * @param {string} s - the input hex string
  * @returns {Uint8Array} the hex-decoded byte array representation of s
  */
-const hex2Bytes = (s: string): Uint8Array => hexDecode(textEncode(s));
+const hexToBytes = (s: string): Uint8Array => hexDecode(stringToBytes(s));
 
-export { bytes2Hex, hex2Bytes, textDecode, textEncode };
+export { bytesToHex, hexToBytes, bytesToString, stringToBytes };
--- lib/security_test.ts
+++ lib/security_test.ts
@@ -1,9 +1,14 @@
 import { assertEquals } from "std/testing/asserts.ts";
-import { bytes2Hex, hex2Bytes, textEncode } from "./security.ts";
+import {
+  bytesToHex,
+  bytesToString,
+  hexToBytes,
+  stringToBytes,
+} from "./security.ts";
 
 Deno.test("round trip hex value", () => {
   const s = "hello world";
-  const h = bytes2Hex(textEncode(s));
+  const h = bytesToHex(stringToBytes(s));
   assertEquals(h, "68656c6c6f20776f726c64");
-  assertEquals(hex2Bytes(h), textEncode(s));
+  assertEquals(bytesToString(hexToBytes(h)), s);
 });
