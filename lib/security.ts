52e6d8935fcf82e79b575646978e25860bee0ab5
Brad Clawsie <brad@b7j0c.org>
2023-02-25 17:53:08 -0600

    clean up tests

--- lib/security.ts
+++ lib/security.ts
@@ -2,7 +2,7 @@ import { crypto } from "https://deno.land/std/crypto/crypto.ts";
 import { toHashString } from "https://deno.land/std/crypto/to_hash_string.ts";
 import { stringToBytes } from "./text.ts";
 
-const sha256Hex = async (s: string): Promise<string> => 
+const sha256Hex = async (s: string): Promise<string> =>
   toHashString(await crypto.subtle.digest("SHA-256", stringToBytes(s)));
 
 export { sha256Hex };
--- lib/text_test.ts
+++ lib/text_test.ts
@@ -9,6 +9,6 @@ import {
 Deno.test("round trip hex value", () => {
   const s = "hello world";
   const h = bytesToHex(stringToBytes(s));
-  assertEquals(h, "68656c6c6f20776f726c64");
-  assertEquals(bytesToString(hexToBytes(h)), s);
+  assertEquals(h, "68656c6c6f20776f726c64", "hex value");
+  assertEquals(bytesToString(hexToBytes(h)), s, "round trip");
 });
